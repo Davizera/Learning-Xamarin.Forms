2cf3cb566df1aa65fca50df5274daad9dd76c053
Davizera <davi.sousa10@gmail.com>
2020-12-21 16:53:58 -0300

    Finalizado primeira parte do curso de Xamarin

--- TestDrive/Views/AgendamentoView.xaml.cs
+++ TestDrive/Views/AgendamentoView.xaml.cs
@@ -13,6 +13,22 @@ namespace TestDrive.Views
 	public partial class AgendamentoView : ContentPage
 	{
 		public Veiculo Carro { get; set; }
+
+		private DateTime _dataAgendamento = DateTime.Today;
+		private TimeSpan _horaAgedamento = DateTime.Now.TimeOfDay;
+		public string Nome { get; set; }
+		public string Telefone { get; set; }
+		public string Email { get; set; }
+		public DateTime DataAgendamento
+		{
+			get { return _dataAgendamento; }
+			set { _dataAgendamento = value; }
+		}
+		public TimeSpan HoraAgendamento
+		{
+			get { return _horaAgedamento; }
+			set { _horaAgedamento = value; }
+		}
 		public AgendamentoView(Veiculo veiculo)
 		{
 			InitializeComponent();
@@ -20,9 +36,17 @@ namespace TestDrive.Views
 			this.BindingContext = this;
 		}
 
-		private void Previous_Page(object sender, EventArgs e)
+		private void Agendar(object sender, EventArgs e)
 		{
-			Navigation.PopAsync();
+			DisplayAlert("Agendamento",
+				string.Format(
+					@"Você, {0}, está agendando o carro, {1}.
+	Seguem seus dados:
+		Nome: {2},
+		Telefone: {3},
+		Email: {4},
+		Dia Agendamento: {5}
+		Hora Agendamento: {6}", Nome, Carro.Nome, Nome, Telefone, Email, DataAgendamento.ToString("dd/MM/yyyy"), HoraAgendamento.ToString(@"hh\:mm")), "Finalizar");
 		}
 	}
 }
--- TestDrive/Views/DetalheView.xaml.cs
+++ TestDrive/Views/DetalheView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,96 @@ namespace TestDrive.Views
 	public partial class DetalheView : ContentPage
 	{
 		public Veiculo Veiculo { get; set; }
+		private const int FREIO_ABS = 800;
+		private const int AR_CONDICIONADO = 1000;
+		private const int MP3_PLAYER = 500;
+		public string TextoFreioABS
+		{
+			get
+			{
+				return string.Format("Freio ABS - {0}", FREIO_ABS.ToString("C", CultureInfo.GetCultureInfo("pt-BR")));
+			}
+		}
+
+		public string TextoArCondicionado
+		{
+			get
+			{
+				return string.Format("Ar Condicionado - {0}", AR_CONDICIONADO.ToString("C", CultureInfo.GetCultureInfo("pt-BR")));
+			}
+		}
+
+		public string TextoMP3Player
+		{
+			get
+			{
+				return string.Format("MP3 Player - {0}", MP3_PLAYER.ToString("C", CultureInfo.GetCultureInfo("pt-BR")));
+			}
+		}
+
+		private bool _temFreioABS;
+		public bool TemFreioABS
+		{
+			get { return _temFreioABS; }
+			set
+			{
+				_temFreioABS = value;
+				if (_temFreioABS)
+					DisplayAlert("Freio ABS", "Incluído", "Fechar");
+				else
+					DisplayAlert("Freio ABS", "Removido", "Fechar");
+
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(ValorTotal));
+			}
+		}
+
+		private bool _temArCondicionado;
+		public bool TemArCondicionado
+		{
+			get { return _temArCondicionado; }
+			set
+			{
+				_temArCondicionado = value;
+				if (_temArCondicionado)
+					DisplayAlert("Ar Condicionado", "Incluído", "Fechar");
+				else
+					DisplayAlert("Ar Condicionado", "Removido", "Fechar");
+
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(ValorTotal));
+			}
+		}
+
+		private bool _temMP3Player;
+		public bool TemMP3Player
+		{
+			get { return _temMP3Player; }
+			set
+			{
+				_temMP3Player = value;
+				if (_temMP3Player)
+					DisplayAlert("MP3 Player", "Incluído", "Fechar");
+				else
+					DisplayAlert("MP3 Player", "Removido", "Fechar");
+
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(ValorTotal));
+			}
+		}
+
+		public string ValorTotal
+		{
+			get
+			{
+				return string.Format("Valor total: {0}",
+					(Veiculo.Preco
+						+ (_temFreioABS ? FREIO_ABS : 0)
+						+ (_temArCondicionado ? AR_CONDICIONADO : 0)
+						+ (_temMP3Player ? MP3_PLAYER : 0)
+						).ToString("C", CultureInfo.GetCultureInfo("pt-BR")));
+			}
+		}
 		public DetalheView(Veiculo veiculo)
 		{
 			InitializeComponent();
